418d47da684fad99c40bda138fb3b32f21eee244
nvaulia <Noviaulia0156@gmail.com>
2026-09-04 23:29:12 +0700

    perbaikan pemanggilan css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil Novi Aulia</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://noviaulia1043.infinityfreeapp.com/style.css?v=2">
 </head>
 
 <body>
